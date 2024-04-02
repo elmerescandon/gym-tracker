9a3e999b98cbbf8860eeb8e95f5ace0f1a1e348b
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-02 17:12:39 -0500

    Fix import formatting and add Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gym-tracker-uwuteam.netlify.app",
+  site: "https://gym-tracker-uwuteam.netlify.app",
+  integrations: [preact()]
 });
